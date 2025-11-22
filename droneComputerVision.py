5b3784852bec788540c738053d23091ccf5d170d
ShouryaP9 <163233738+ShouryaP9@users.noreply.github.com>
2025-11-22 15:49:41 -0800

    Add files via upload

---
+++ droneComputerVision.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+# connects to camera
+cam = cv2.VideoCapture(0)
+# Defines the range of colors that should be detected (in this case, shades of red)
+colorThreshold = 143
+#Minimum area of a detected object for it to be circled
+minArea = 6000
+largestShape = None
+#Used for reducing noise and filling holes in object detection
+kernel = np.ones((3, 3), np.uint8)
+
+#Runs the code inside continuously
+while True:
+    #reads the camera input
+    _, frame = cam.read()
+
+    #blurs the camera input to reduce noise and glare
+    blurredFrame = cv2.GaussianBlur(frame, (7,7), 0)
+
+    # Converts to LAB color space (better for color detection)
+    lab = cv2.cvtColor(blurredFrame, cv2.COLOR_BGR2LAB)
+
+    #splits the LAB channels and assigns A (red and green detection) to a variable
+    _, A, _ = cv2.split(lab)
+
+    # Creates a mask to show where red is detected
+    _, mask = cv2.threshold(A, colorThreshold, 255, cv2.THRESH_BINARY)
+
+    #gets the shapes(contours) of red detected by the mask, using the basic contour finding mode and method
+    shapes, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    #reduces noise
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    #fills holes in detected objects
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    if len(shapes) > 0:
+        largestShape = max(shapes, key=cv2.contourArea)
+
+    if largestShape is not None:
+        if cv2.contourArea(largestShape) >= minArea:
+
+            #returns a dictionary of moments
+            moment = cv2.moments(largestShape)
+
+            if moment["m00"] != 0:
+                #x coordinate of center of detected object
+                x = int(moment["m10"] / moment["m00"])
+                #y coordinate of center of detected object
+                y = int(moment["m01"] / moment["m00"])
+
+                #circles the detected object with a circle of radius 6, color green, and thickness of 6
+                cv2.circle(frame, (x, y), 100, (0, 255, 0), 7)
+                cv2.circle(mask, (x, y), 100, (0, 255, 0), 7)
+
+    # Shows the normal camera feed and the mask
+    cv2.imshow("camera", frame)
+    cv2.imshow("red detection", mask)
+
+    # Breaks the while loop if escape is pressed
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
